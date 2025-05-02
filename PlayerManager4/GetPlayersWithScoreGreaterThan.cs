c0ab12ac73d3e29f7db8fd44b89f5addfbefd2d7
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-05-02 02:38:06 +0100

    Added PlayerManager4

---
+++ PlayerManager4/GetPlayersWithScoreGreaterThan.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace PlayerManager4
+{
+    public class PlayerManager
+    {
+        private List<Player> players;
+
+        public PlayerManager()
+        {
+            players = new List<Player>();
+        }
+
+        public void AddPlayer(string name, int score)
+        {
+            players.Add(new Player(name, score));
+            Console.WriteLine($"\nPlayer {name} added!\n");
+        }
+
+        public IEnumerable<Player> GetPlayers()
+        {
+            foreach (Player player in players)
+            {
+                yield return player;
+            }
+        }
+
+        /// <summary>
+        /// Get players with a score higher than a given value.
+        /// </summary>
+        /// <param name="minScore">Minimum score players should have.</param>
+        /// <returns>
+        /// An enumerable of players with a score higher than the given value.
+        /// </returns>
+        public IEnumerable<Player> GetPlayersWithScoreGreaterThan(int minScore)
+        {
+            foreach (Player player in players)
+            {
+                if (player.Score > minScore)
+                {
+                    yield return player; // use yield return to return each player one by one
+                }
+            }
+        }
+    }
+}
---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace PlayerManager4
+{
+    public class Player : IComparable<Player>
+    {
+        //readonly, no use of set;
+        public string Name { get; }
+        public int Score { get; }
+
+        
+
+        //constructor, aka same name as class, no return type
+        public Player(string name, int score)
+        {
+            Name = name;  //no need for "this." because no other variable with the same name
+            Score = score;
+        }
+
+
+        public int CompareTo(Player other)
+        {
+            if (other == null) return 1; 
+            return Score.CompareTo(other.Score); //compare scores
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Score: {Score}";
+        }
+    }
+}
